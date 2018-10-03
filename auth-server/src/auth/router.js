4dff78a87cd8839af741b2e3072fa3e72e243f9e
alexlhanson <alexlhanson@gmail.com>
2018-10-03 12:04:01 -0700

    Add front end for requests

--- auth-server/src/auth/router.js
+++ auth-server/src/auth/router.js
@@ -1,9 +1,22 @@
 'use strict';
 
 import express from 'express';
+import authenticator from '../middleware/authenticator';
+import User from './user';
 
 const router = express.Router();
 
-a
+router.post('/signup', (req, res, next) => {
+  console.log(req.body);
+  let user = new User(req.body);
+  user.save()
+    .then(user => res.send(user.generateToken()))
+    .catch(next);
+});
+
+router.get('/signin', authenticator, (req, res, next) => {
+  res.cookie('Token', req.token);
+  res.send('login successful');
+});
 
 export default router;
---
+++ auth-server/src/auth/user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre('save', function (next) {
+  bcrypt.hash(this.password, 19)
+    .then(hashed => {
+      this.password = hashed;
+
+      next();
+    })
+    .catch(err => { throw err; });
+});
+
+userSchema.methods.generateToken = function (){
+  return jwt.sign({id:this._id}, process.env.APP_SECRET || 'secret');
+};
+
+userSchema.statics.authenticate = function(auth) {
+  return this.findOne({username: auth.username})
+    .then(user => user && user.comparePassword(auth.password))
+    .catch(err => err);
+};
+
+userSchema.methods.comparePassword = function(password){
+  return bcrypt.compare(password, this.password)
+    .then(valid => valid ? this : null);
+};
+
+export default mongoose.model('users', userSchema);
